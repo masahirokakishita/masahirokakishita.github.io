dba29e541267af92268fe8a9c0fcb2b88be8f32d
masahirokakishita <masahiro.kakishita@gmail.com>
2015-09-27 15:28:05 +0900

    15092710

--- testmidi.js
+++ testmidi.js
@@ -1,9 +1,37 @@
 (function(ext) {
 
+	var m=null;
+	var inputs=null;
+	var input=null;
+	var outputs=null;
+	var output=null;
+	var input_device=0;
+	var output_device=0;
+
 	navigator.requestMIDIAccess().then( success, failure );
 
 	function success(midiAccess) {
+		m=midiAccess;
+
+		if (typeof m.inputs === "function") {
+			inputs=m.inputs();
+			outputs=m.outputs();
+		} else {
+			var inputIterator = m.inputs.values();
+			inputs = [];
+			for (var o = inputIterator.next(); !o.done; o = inputIterator.next()) {
+				inputs.push(o.value)
+			}
+
+			var outputIterator = m.outputs.values();
+			outputs = [];
+			for (var o = outputIterator.next(); !o.done; o = outputIterator.next()) {
+				outputs.push(o.value)
+			}
+		}
 		console.log("You can use MIDI API!");
+		console.log("input device=",inputs.length);
+		console.log("output device=",outputs.length);
 	}
 
 	function failure(error) {
@@ -19,12 +47,22 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    function m_midiout(ch, note, vel){
+	console.log(ch,note,vel);
+    };
+
+    ext.midiout = function(ch, note, vel) {
+        m_midiout(ch, note, vel);
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+      [' ', 'MIDI OUT %n %n %n', 'midiout', 10, 36, 80],
+      ['-'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('MIDI extension', descriptor, ext);
 })({});
