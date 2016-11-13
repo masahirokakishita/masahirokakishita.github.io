3d82b1f90754d15d5bf1d3f34648dfdb89cda8b8
Masahiro Kakishita <masahiro.kakishita@gmail.com>
2016-11-13 16:00:05 +0900

    20161113

--- WebMIDI/gamepadapi.js
+++ WebMIDI/gamepadapi.js
@@ -23,36 +23,19 @@
 /* for Web MIDI API */
 /* -------------------------------------------------------------------------	*/
 
+	var pad=null;
+
 	function runAnimation()
 	{
 		window.requestAnimationFrame(runAnimation);
 	    var gamepad_info = '';
 		var gamepads = navigator.getGamepads();
 		var gamepad_num =gamepads.length;
+		pad = new Array(gamepad_num);
 
-		var i, b, a;
-		var no_pad = true;
-		for (i=0; i<gamepad_num; i++) {
-			var pad = gamepads[i];
-			if(pad!=null) {
-				no_pad = false;
-				console.log(pad.buttons[1].pressed, pad.buttons[1].value);
-//				gamepad_info += "Gamepad[" + i + "]<br>";
-
-				for (b=0; b<pad.buttons.length; b++) {
-//					gamepad_info += "buttons[" + b + "]=" + pad.buttons[b].pressed + " : " + pad.buttons[b].value + "<br>";
-				}
-
-				for (a=0; a<pad.axes.length; a++) {
-//					gamepad_info += "axis[" + a + "]=" + pad.axes[a] + "<br>";
-				}
-			}
+		for (var i=0; i<gamepad_num; i++) {
+			if(gamepads[i]!=null) pad[i] = gamepads[i];
 		}
-		
-		if (no_pad) {
-//			gamepad_info = '画面をクリックした後にゲームパッドのボタンを押して下さい';
-		}
-		document.getElementById('log').innerHTML = gamepad_info;
 	}
 
 	window.requestAnimationFrame(runAnimation);
@@ -75,17 +58,109 @@
     };
 
 /* -------------------------------------------------------------------------	*/
-// GET CC
-	ext.s_getcc = function() {
-		// Reset alarm_went_off if it is true, and return true
-		// otherwise, return false.
-//		if (mCC_change_event === true) {
+// BUTTON X- Y- A- B-
+	ext.s_getXbtn = function() {
+		return pad[0].buttons[2].pressed;
+	};
+
+	ext.s_getYbtn = function() {
+		return pad[0].buttons[3].pressed;
+	};
+
+	ext.s_getAbtn = function() {
+		return pad[0].buttons[0].pressed;
+	};
+
+	ext.s_getBbtn = function() {
+		return pad[0].buttons[1].pressed;
+	};
+
+//Set Value
+	ext.s_Xval = function() {
+		return (pad[0].buttons[2].value);
+	};
+
+	ext.s_Yval = function() {
+		return (pad[0].buttons[3].value);
+	};
+
+	ext.s_Aval = function() {
+		return (pad[0].buttons[0].value);
+	};
+
+	ext.s_Bval = function() {
+		return (pad[0].buttons[1].value);
+	};
+
+/* -------------------------------------------------------------------------	*/
+// BUTTON L1- L2- R1- R2-
+	ext.s_getL1btn = function() {
+		return pad[0].buttons[4].pressed;
+	};
+
+	ext.s_getL2btn = function() {
+		return pad[0].buttons[5].pressed;
+	};
+
+	ext.s_getR1btn = function() {
+		return pad[0].buttons[6].pressed;
+	};
+
+	ext.s_getR2btn = function() {
+		return pad[0].buttons[7].pressed;
+	};
+
+//Set Value
+	ext.s_L1val = function() {
+		return (pad[0].buttons[4].value);
+	};
+
+	ext.s_L2val = function() {
+		return (pad[0].buttons[5].value);
+	};
+
+	ext.s_R1val = function() {
+		return (pad[0].buttons[6].value);
+	};
+
+	ext.s_R2val = function() {
+		return (pad[0].buttons[7].value);
+	};
+
+/* -------------------------------------------------------------------------	*/
+// BUTTON L1- L2- R1- R2-
+	var hval=0;
+	var vval=0;
+
+	ext.s_getHbtn = function() {
+		var fh=Math.floor(pad[0].axes[0]);
+		if(hval!=fh){
+			hval=fh;
 			return true;
-//       }
-       return false;
+		}
+		else return false;
+	};
+
+	ext.s_getVbtn = function() {
+		var fv=Math.floor(pad[0].axes[1]);
+		if(vval!=fv){
+			vval=fv;
+			return true;
+		}
+		else return false;
+	};
+
+//Set Value
+	ext.s_Hval = function() {
+		return (hval);
+	};
+
+	ext.s_Vval = function() {
+		return (vval);
 	};
 
 
+/* -------------------------------------------------------------------------	*/
 // Set CC Value
 // ccnum: Control Change Number
 	ext.s_ccin = function(ccnum) {
@@ -140,19 +215,43 @@
 	// Block and block menu descriptions
 	var descriptor = {
 		blocks: [
-			[' ', 'AAA MIDI %n %n %n', 'midiout', 10, 36, 80],
-			['h', 'BBB CC', 's_getcc'],
-			['r', 'CC %n', 's_ccin',30],
-			['h', 'GET NOTE ON', 's_getnoteon'],
-			['h', 'KEY ON', 's_getkeyon'],
-			['h', 'KEY OFF', 's_getkeyoff'],
-			['r', 'NOTE', 's_note'],
-			['r', 'VEL', 's_vel'],
+//			[' ', 'AAA MIDI %n %n %n', 'midiout', 10, 36, 80],
+			['h', 'X-Button', 's_getXbtn'],
+			['h', 'Y-Button', 's_getYbtn'],
+			['h', 'A-Button', 's_getAbtn'],
+			['h', 'B-Button', 's_getBbtn'],
+
+			['r', 'X-Val', 's_Xval'],
+			['r', 'Y-Val', 's_Yval'],
+			['r', 'A-Val', 's_Aval'],
+			['r', 'B-Val', 's_Bval'],
+
+			['h', 'L1-Button', 's_getL1btn'],
+			['h', 'L2-Button', 's_getL2btn'],
+			['h', 'R1-Button', 's_getR1btn'],
+			['h', 'R2-Button', 's_getR2btn'],
+
+			['r', 'L1-Val', 's_L1val'],
+			['r', 'L2-Val', 's_L2val'],
+			['r', 'R1-Val', 's_R1val'],
+			['r', 'R2-Val', 's_R2val'],
+
+			['h', 'Horizontal', 's_getHbtn'],
+			['h', 'Vartical', 's_getVbtn'],
+			['r', 'H-Val', 's_Hval'],
+			['r', 'V-Val', 's_Vval'],
+
+//			['r', 'CC %n', 's_ccin',30],
+//			['h', 'GET NOTE ON', 's_getnoteon'],
+//			['h', 'KEY ON', 's_getkeyon'],
+//			['h', 'KEY OFF', 's_getkeyoff'],
+//			['r', 'NOTE', 's_note'],
+//			['r', 'VEL', 's_vel'],
 			['-'],
 		]
 	};
 
     // Register the extension
-    ScratchExtensions.register('Web MIDI API extension', descriptor, ext);
+    ScratchExtensions.register('GAME PAD API extension', descriptor, ext);
 })({});
 
