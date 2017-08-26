2f43b86a85049d806f16ecd2692fa57a674c68c4
masahirokakishita <masahiro.kakishita@gmail.com>
2017-08-27 07:29:32 +0900

    test

--- testmidiR2.js
+++ testmidiR2.js
@@ -12,17 +12,12 @@
     // final argument. This should be called to indicate that the block can
     // stop waiting.
     ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
+            ['b', 'wait aaa', 'wait_random'],
         ]
     };
 
